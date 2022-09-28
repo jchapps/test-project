e24800ad915fc455c92da20939eafdcbd1ebda73
Josh Chappelow <jdchappelow@gmail.com>
2022-09-28 21:51:08 +0900

    search bar styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import '../styles/App.scss';
 import '../styles/gifGrid.scss';
+import '../styles/searchbar.scss';
+import '../styles/gif.scss';
 import SearchBar from '../components/Searchbar'
 import GifList from './GifList';
 
 function App() {
   return (
     <div>
-      <SearchBar/>
+      <div className="searchbar">
+       <SearchBar/>
+      </div>
       <GifList/>
     </div>
 )};
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,19 +3,19 @@ import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 
 const SearchBar = () => (
-  <form>
-    <TextField
-      id="search-bar"
-      className="text"
-      label="Search for a GIF"
-      variant="outlined"
-      placeholder="GIF "
-      size="medium"
-    />
-    <IconButton type="submit" aria-label="search">
-      <SearchIcon style={{ fill: "blue" }} />
-    </IconButton>
-  </form>
+    <form>
+      <TextField
+        id="search-bar"
+        className="text"
+        label="Search for a GIF"
+        variant="outlined"
+        placeholder="GIF "
+        size="medium"
+      />
+      <IconButton type="submit" aria-label="search" className="icon">
+        <SearchIcon />
+      </IconButton>
+    </form>
 );
 
 export default SearchBar;
