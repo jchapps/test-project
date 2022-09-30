c7eb3bddcb96702dbe7c893214c38d6932cd655b
Josh Chappelow <jdchappelow@gmail.com>
2022-09-30 18:51:21 +0900

    webpack warning removed

--- src/components/GifList.jsx
+++ src/components/GifList.jsx
@@ -13,13 +13,3 @@ const GifList = (props) => {
 };
 
 export default GifList;
-
-{/* <Gif gifId="3o85xsGXVuYh8lM3EQ"/>
-      <Gif gifId="riwAaDlaA3zQQ"/>
-      <Gif gifId="aFTt8wvDtqKCQ"/>
-      <Gif gifId="bGgsc5mWoryfgKBx1u"/>
-      <Gif gifId="Z8k6qo0AVIsPdROQNd"/>
-      <Gif gifId="1Bh2nNww4IFE9rQZ3S"/>
-      <Gif gifId="w36LqLo57gmvXa7wjf"/>
-      <Gif gifId="3o7abKhOpu0NwenH3O"/>
-      <Gif gifId="SRn9TUPpFuQAU"/> */}
