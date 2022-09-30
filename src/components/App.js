b5f0f09bcfceb850d097643536577c628c031758
Josh Chappelow <jdchappelow@gmail.com>
2022-09-30 16:02:29 +0900

    search logic calls api

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,35 @@ import '../styles/gifGrid.scss';
 import '../styles/searchbar.scss';
 import SearchBar from '../components/Searchbar'
 import GifList from './GifList';
+import React, { useState } from "react";
+
+const giphy = require("giphy-api")({
+  apiKey: "8dq7ojpO6TcW65DTXXd5aHG5iqeKib0D",
+  https: true,
+});
 
 function App() {
-  const gifIds = ['3o85xsGXVuYh8lM3EQ','riwAaDlaA3zQQ','aFTt8wvDtqKCQ', 'bGgsc5mWoryfgKBx1u', 'Z8k6qo0AVIsPdROQNd', '1Bh2nNww4IFE9rQZ3S', 'w36LqLo57gmvXa7wjf','3o7abKhOpu0NwenH3O', 'SRn9TUPpFuQAU']
+  const [gifIdList, setGifIds] = useState (['3o85xsGXVuYh8lM3EQ','riwAaDlaA3zQQ','aFTt8wvDtqKCQ', 'bGgsc5mWoryfgKBx1u', 'Z8k6qo0AVIsPdROQNd', '1Bh2nNww4IFE9rQZ3S', 'w36LqLo57gmvXa7wjf','3o7abKhOpu0NwenH3O', 'SRn9TUPpFuQAU'])
+  const fetchGiphy = (keyword) => {
+    giphy.search(
+      {
+        q: keyword,
+        rating: "g",
+        limit: 9,
+      },
+      (err, res) => {
+        const gifIdsArr = res.data.map((gif) => gif.id)
+        setGifIds(gifIdsArr)
+      }
+    );
+  };
+
   return (
     <div>
       <div className="searchbar">
-       <SearchBar/>
+       <SearchBar fetchGiphy={fetchGiphy} />
       </div>
-      <GifList gifIds={gifIds}/>
+      <GifList gifIds={gifIdList}/>
     </div>
 )};
 
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,7 +2,12 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 
-const SearchBar = () => (
+const SearchBar = (props) => {
+    const handleChange = (event) => {
+      const { fetchGiphy } = props;
+      fetchGiphy(event.currentTarget.value);
+    };
+    return (
     <form>
       <TextField
         id="search-bar"
@@ -11,11 +16,12 @@ const SearchBar = () => (
         variant="outlined"
         placeholder="GIF "
         size="medium"
+        onChange={handleChange}
       />
       <IconButton type="submit" aria-label="search" className="icon">
         <SearchIcon />
       </IconButton>
     </form>
-);
+)};
 
 export default SearchBar;
